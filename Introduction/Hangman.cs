ff032b3e665695f45164bd4f34ffb7e75ce4b38c
David Tanzer <business@davidtanzer.net>
2017-06-15 07:05:51 +0200

    Initial import

---
+++ Introduction/Hangman.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduction
+{
+    public class Hangman
+    {
+        public void Foo()
+        {
+
+        }
+
+        static void Main()
+        {
+
+        }
+    }
+}
---
+++ IntroductionTests/HangmanTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Introduction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduction.Tests
+{
+    [TestClass()]
+    public class HangmanTests
+    {
+        [TestMethod()]
+        public void FooTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
