8a74f6ebdb2e4eb39f3b21f88b33a40496b59ea8
David Tanzer <business@davidtanzer.net>
2017-06-15 08:35:25 +0200

    Added projects for second exercise (start with rule set)

---
+++ StartWithRuleSet/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartWithRuleSet
+{
+    public class Hangman
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ StartWithRuleSetTests/HangmanTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StartWithRuleSet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartWithRuleSet.Tests
+{
+    [TestClass()]
+    public class HangmanTests
+    {
+        [TestMethod()]
+        public void SomeTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
