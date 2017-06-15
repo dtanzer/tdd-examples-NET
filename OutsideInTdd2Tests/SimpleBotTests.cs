469a1059b1fc128cc86aee54d2b0d844d0ec35df
David Tanzer <business@davidtanzer.net>
2017-06-15 08:56:04 +0200

    Merged empty examples from master

---
+++ OutsideInTdd2Tests/SimpleBotTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OutsideInTdd2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutsideInTdd2.Tests
+{
+    [TestClass()]
+    public class SimpleBotTests
+    {
+        [TestMethod()]
+        public void SomeTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ OutsideInTddTests/SimpleBotTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OutsideInTdd;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutsideInTdd.Tests
+{
+    [TestClass()]
+    public class SimpleBotTests
+    {
+        [TestMethod()]
+        public void SomeTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
