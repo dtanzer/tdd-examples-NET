c8b9bb62c3aca40953e6bc1d79a3b138f49e3b5b
David Tanzer <business@davidtanzer.net>
2017-06-15 08:54:06 +0200

    Added exercises for outside-in TDD

---
+++ OutsideInTdd2/SimpleBot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutsideInTdd2
+{
+    public class SimpleBot
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
